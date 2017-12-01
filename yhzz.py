6f9db8e7ab6f4f17f67a736a8ae1f58fca8b9776
GYbin <gouyabin@gmail.com>
2017-12-01 23:51:15 +0800

    yhzz

--- yhzz.py
+++ yhzz.py
@@ -41,6 +41,7 @@ def headers(proto="get"): #发送的头文件定义
         return -1
 
 def laft_lan(html): #获取侧边栏选项地址
+    global yhzzxq
     laft_bs={'gk':'概 况','jz':'建 筑','yj':'研 究',
               'cc':'船 厂','fy':'防 御','lm':'联 盟',
               'jd':'舰 队','xx':'消 息','ybyh':'外部银河',
@@ -63,29 +64,30 @@ def laft_lan(html): #获取侧边栏选项地址
                 pattern = r'<a href=\"(.*?)" class="menubutton'
                 lafturl_tmp = re.findall(pattern,urldata_tmp)
                 laft_url[laft_bskey]=lafturl_tmp[0]
-    return laft_url
+    yhzzxq['laft_url'] = laft_url
+    return 0
 
 def get_laft_lan(laft_data):#打开侧边栏地址，返回html页面
 #    if not isinstance(laft_url_dict,dict):
 #        print "传入写入参数需要使用Dict类型"
     time.sleep(2)
     global login_Ref
-    url_tmp=usercon['geturl']+laft_lan_url[laft_data]
+    url_tmp=usercon['geturl']+laft_data
+    #usercon['geturl'] 地址前缀
     login_Ref = url_tmp
+    print "打开URL：",url_tmp
     html_txt = res.get(url_tmp,headers=headers('get'))
     html_txt.encoding = 'gbk'
     return html_txt.text
-    #if laft_data=='gk':
-    #    get_laft_langk(html_txt.text)
-    #elif laft_data=='jz':
-    #    return html_txt.text
-    #    get_laft_lanjz(html_txt.text)
-    #else:
-    #    return html_txt.text
-    #get_laft_langk(html_txt.text)
-
 
 def get_laft_lanjz(get_html):#获取建筑信息
+    pattern = r'<option selected="selected" value="(.*?)">(.*?)&nbsp;\[(.*?)\]&nbsp;&nbsp;</option>'
+    xqgk_tmpwz = re.findall(pattern,get_html)#获取星球位置信息
+    print '当前星球位置：'+xqgk_tmpwz[0][2],'名称:' + xqgk_tmpwz[0][1]
+    for xqname in yhzzxq:
+        if yhzzxq.get(xqname).get('xqwz')==xqgk_tmpwz[0][2] and yhzzxq.get(xqname).get('xqname')==xqgk_tmpwz[0][1]:
+            print '当前星球:'+ xqname
+            break
     pattern = r'<td class="l">(.*?)ont></a></td>'
     zyjz_tmp = re.findall(pattern,get_html,re.S)
     for zyjz_tmpxx in zyjz_tmp:
@@ -104,6 +106,7 @@ def get_laft_lanjz(get_html):#获取建筑信息
             print '资源不够'
             continue
         print '可以升级'
+
 def ifziyuanfs(tmp):#判断是否都是正数
     for ifzy in tmp:
         if not int(ifzy.strip().replace(',','')) >= 0:
@@ -158,7 +161,7 @@ html_txt = res.get(login_html,headers=headers('get'))
 html_txt.encoding = 'gbk'
 html_data = html_txt.text
 laft_lan_url=laft_lan(html_data)
-tmp = get_laft_lan('gk')
+tmp = get_laft_lan(yhzzxq['laft_url']['gk'])
 get_laft_langk(tmp)
 
 
